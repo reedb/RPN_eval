34831226d522527cdc1a4ed390345dba90fb2094
reedb <reedb@reedbement.com>
2019-01-06 09:39:22 -0800

    Finish integration function using RPN expression

--- integrate.py
+++ integrate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import math  as m
+
 import eval_rpn as e
 
 '''
@@ -8,24 +8,22 @@ Use the extended trapezoidal rule to calculate integrals.
 From "Computational Physics" by Mark Newman
 http://www-personal.umich.edu/~mejn/cp/chapters/int.pdf
 
+def f(x): return x**4 - 2*x + 1
+
 '''
-def f(x):
-    return x**4 - 2*x + 1
 
-N = 5
-a = 0.0
-b = 2.0
-h = (b-a)/N
+# The following string is the RPN equivelent of above.
+#
+fun_rpn = 'x 4 pow 2 x * - 1 +'
 
-s = 0.5*f(a) + 0.5*f(b)
-for k in range(1,N):
-    s += f(a+k*h)
+def ExtTrapIntegral(fun_rpn, a, b, N):
+    h = (b - a) / N
+    s = 0.5 * e.eval_rpn(fun_rpn, a) + 0.5 * e.eval_rpn(fun_rpn, b)
+    for k in range(1, N):
+        s += e.eval_rpn(fun_rpn, (a + k * h))
+    return h*s
 
-print(h*s)
+print "Calculated: " +  str(ExtTrapIntegral(fun_rpn, 0.0, 2.0, 1000))
+print "Exact: 4.4"
 
 
-
-if __name__ == '__main__':
-#    assert eval_rpn('10 3 5 + *') == eval_rpn('3 5 + 10 *')
-#    qsimp("", 0, 0)
-    print "Tests pass!"
--- test_exp.py
+++ test_exp.py
@@ -11,5 +11,3 @@ assert f(0.0) == e.eval_rpn(expres, 0.0)
 assert f(1.0) == e.eval_rpn(expres, 1.0)
 assert f(2.0) == e.eval_rpn(expres, 2.0)
 print "Tests pass!"
-
-#print e.eval_rpn('x 4 pow 2 x * - 1 +', 2)
